6ed7b6499c2188b40d22dede7362082aeffd0e11
matthewdouglasmartin <70335390+matthewdouglasmartin@users.noreply.github.com>
2020-12-10 18:53:58 -0600

    13.6.6

--- Earthquakes_last7days/static/js/logicStep4.js
+++ Earthquakes_last7days/static/js/logicStep4.js
@@ -106,5 +106,5 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     }).addTo(earthquakes);
 
         // Then we add the earthquake layer to our map.
-        earthquakes.addTo(map);
+    earthquakes.addTo(map);
 });
